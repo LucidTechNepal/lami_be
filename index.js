dd90326161783ed74a5fd60ae368995979e7d9bd
Ashish Pokhrel <39693277+ashishpokhrel123@users.noreply.github.com>
2024-03-20 09:37:37 +0545

    fix

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const serviceAccount = {
   "type": "service_account",
   "project_id": "test-notification-83b73",
   "private_key_id": process.env.FIREBASE_KEY_ID,
-  "private_key": process.env.FIREBASE_KEY,
+  "private_key": process.env.FIREBASE_KEY.replace(/\\n/g, '\n'),
   "client_email": "firebase-adminsdk-czuur@test-notification-83b73.iam.gserviceaccount.com",
   "client_id": "105793570775823189629",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
